9763bf2a052d290b338d3c5496ce9bca1c34a31f
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-05-04 20:01:40 +1000

    feat: Initial commit.

---
+++ src/Program.cs
@@ -0,0 +1,7 @@
+﻿using var client = new HttpClient();
+
+Console.WriteLine("Enter username:");
+var username = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Enter password:");
+var password = Console.ReadLine() ?? string.Empty;
